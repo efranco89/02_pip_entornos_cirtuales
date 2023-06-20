61164fdb2b88dce2c2e266554fcc1d397319dd68
Enrique Franco <luis.franco@wizeline.com>
2023-06-20 13:27:11 -0500

    The fastapi was added

--- web-server/main.py
+++ web-server/main.py
@@ -1,4 +1,20 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get('/')
+def get_list():
+  return [1,2,3,4]
+
+@app.get('/contact')
+def get_list():
+  return { 'name': 'Platzi' }
+
+@app.get('/example', response_class=HTMLResponse)
+def get_list():
+  return "<h1>Hola Soy una Pagina</h1>"
 
 def run():
   store.get_categories()
